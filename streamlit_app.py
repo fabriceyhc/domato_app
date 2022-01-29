8ac45d38c7bfe55447aed38d1f7e04e6108c8f21
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2022-01-29 14:54:22 -0800

    adding domato repo and fleshing out streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+import subprocess as sp
+from bs4 import BeautifulSoup
+import glob
+
+output_dir = "./domato/test_html_files"
+
+st.text('How many tests would you like to generate with Domato?')
+
+no_of_files = st.number_input(
+	label='Number of html files',
+	min_value=0,
+	max_value=None,
+	value=10
+	)
+
+# generate tests
+
+cmd = 'python domato/generator.py'
+cmd += ' --output_dir ' + output_dir
+cmd += ' --no_of_files ' + repr(no_of_files)
+sp.call(cmd, shell=True)
+
+html_files = glob.glob(output_dir + '**/*.html', recursive=True)
+
+html_strings = []
+for html_file in html_files:
+    with open(html_file) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        html_strings.append(soup.decode_contents())
+
+
+components.html(html_strings[0].decode_contents())
 
