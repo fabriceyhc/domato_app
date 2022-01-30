a8f355de3a1795a26dbe71294fe43b284cc34606
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2022-01-29 17:12:34 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ def main():
 
         html_string = load_html_file(st.session_state.html_files[st.session_state.html_file_num])
         
-        col1, col2, col3 = st.columns(3)
+        col1, col2, col3, col4, col5 = st.columns(5)
         components.html(
                 html=html_string,
                 width=600,
@@ -91,12 +91,12 @@ def main():
             )
 
         with col1:
-            st.button('Next', on_click=increment_html_file_num, disabled=disable_next_button())
-        with col2:
+            st.button('Previous', on_click=decrement_html_file_num, disabled=disable_prev_button())
+        with col3:
             with open('./domato_test_suite.zip', 'rb') as f:
                 st.download_button('Download All', f, file_name='domato_test_suite.zip')
-        with col3:
-            st.button('Previous', on_click=decrement_html_file_num, disabled=disable_prev_button())
+        with col5:
+            st.button('Next', on_click=increment_html_file_num, disabled=disable_next_button())
 
         with st.expander("See HTML Code"):
             st.download_button(
